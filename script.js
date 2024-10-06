1bc519a8805abf9b74ae30d2ea084945a55e172f
Bhumika Gupta <142495749+bhumika2512@users.noreply.github.com>
2024-10-06 18:27:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+gsap.from("#img1",{
+    opacity:0,
+    duration :2,
+    delay: 0.4
+})
+gsap.from("#img2",{
+    opacity:0,
+    duration :2,
+    delay: 0.4
+})
+gsap.from("#img3",{
+    opacity:0,
+    duration :2,
+    delay: 0.4
+})
+gsap.from("#main h1",{
+    opacity:0,
+    duration :2,
+    delay: 0.4
+})
